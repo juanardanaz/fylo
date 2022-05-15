3fac961ccbbb119d54528bdd2fc5516e6c002a19
Juan <juancruzardanaz@gmail.com>
2022-05-15 20:38:31 -0300

    email validation and design adjustment

--- script/main.js
+++ script/main.js
@@ -9,3 +9,32 @@ function scrollHeader(){
     }
 }
 window.addEventListener('scroll', scrollHeader);
+
+/*=============== EMAIL ADDRESS INPUT ===============*/
+function validation() {
+    let form = document.getElementById('form');
+    let email = document.getElementById('email').value;
+
+    let text = document.getElementById('text');
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+    
+    if (email.match(pattern)) {
+        form.classList.add('valid')
+        form.classList.remove('invalid')
+        text.innerHTML = 'Your email address is valid'
+
+        text.style.color = '#00ff00'
+    }   else {
+        form.classList.remove('valid')
+        form.classList.add('invalid')
+        text.innerHTML = 'Please enter a valid email address'
+        text.style.color = '#ff0000'
+    }
+
+    if (email == '') {
+        form.classList.remove('valid')
+        form.classList.remove('invalid')
+        text.innerHTML = ''
+        text.style.color = '#00ff00'
+    }
+}
